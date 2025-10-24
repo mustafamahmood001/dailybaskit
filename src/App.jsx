c7a8237428cef078eea0490a854185435f9faf26
Mustafa Mahmood <mustafamahmood001@outlook.com>
2025-10-24 15:19:22 +0500

    Updated UI and added assets, pages, and context files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router'
 import React from 'react'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
 
 const App = () => {
   return (
-    <div><h1 className='text-red-400'>Hello</h1>
-
-
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Products" element={<Products/>}/>
+          <Route path="/Product/:id" element={<ProductDetail/>}/>
+          <Route path="/Cart" element={<Cart/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { Children, createContext } from "react";
+
+export const AppContext= createContext(null);
+
+const AppContextProvider=({children}) => {
+    const value = {};
+    return (
+        <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+    );
+}
+
+export default AppContextProvider;
